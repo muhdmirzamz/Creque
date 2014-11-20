ac7456312ef13ba80ef47adb7d0017adef12cba0
muhdmirzamz <muhdmirzamz@gmail.com>
2014-11-20 23:02:01 +0800

    initial commit

---
+++ Creque/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Creque
+//
+//  Created by Muhd Mirza on 20/11/14.
+//  Copyright (c) 2014 Muhd Mirza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
